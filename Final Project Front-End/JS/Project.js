7ace659450e0ad46b5313d0e0c3c8b4279f1b699
KhaledAl-Smadi003 <sriheen20@gmail.com>
2026-07-05 20:43:56 +0300

    Delete Final Project Front-End directory

--- Final Project Front-End/JS/Project.js
+++
@@ -1,7 +0,0 @@
-if (localStorage.getItem("isLoggedIn") !== "true") {
-    window.location.href = "Login.html";
-}
-function logout() {
-    localStorage.clear();
-    window.location.href = "Login.html";
-}
